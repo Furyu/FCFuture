a9651981977ea0aa6e6ae23144cd1be4b8bfe152
KUOKA Yusuke <ykuoka@gmail.com>
2012-01-13 10:13:13 +0900

    Tests for typical use cases.

--- FCFutureTests/FCFutureTests.h
+++ FCFutureTests/FCFutureTests.h
@@ -2,8 +2,8 @@
 //  FCFutureTests.h
 //  FCFutureTests
 //
-//  Created by 九岡 佑介 on 01/12/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by KUOKA Yusuke on 01/12/12.
+//  Copyright (c) 2012 Furyu. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
--- FCFutureTests/FCFutureTests.m
+++ FCFutureTests/FCFutureTests.m
@@ -2,18 +2,19 @@
 //  FCFutureTests.m
 //  FCFutureTests
 //
-//  Created by 九岡 佑介 on 01/12/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by KUOKA Yusuke on 01/12/12.
+//  Copyright (c) 2012 Furyu. All rights reserved.
 //
 
 #import "FCFutureTests.h"
+#import "FCFuture.h"
 
 @implementation FCFutureTests
 
 - (void)setUp
 {
     [super setUp];
-    
+
     // Set-up code here.
 }
 
@@ -24,9 +25,69 @@
     [super tearDown];
 }
 
-- (void)testExample
+// The callback-block should be called immediately after -[FTPromise onRedeem:] if the promise has been already redeemed.
+- (void)testFutureRedeemFirst
 {
-    STFail(@"Unit tests are not implemented yet in FCFutureTests");
+    NSCondition *condition = [[[NSCondition alloc] init] autorelease];
+    FCPromise *promise = [[[FCPromise alloc] init] autorelease];
+
+    [promise redeem:@"value1"];
+    
+    [promise onRedeem:^(id value) {
+        STAssertEqualObjects(value, @"value1", @"It should be called back with the redeemed value. value = %@", value);
+    }];
+}
+
+// The callback-block should be called immediately after -[FTPromise redeem:] if the block has been already set.
+- (void)testFutureSetCallbackFirst
+{
+    FCPromise *promise = [[[FCPromise alloc] init] autorelease];
+
+    [promise onRedeem:^(id value) {
+        STAssertEqualObjects(value, @"value2", @"It should be called back with the redeemed value. value = %@", value);
+    }];
+
+    [promise redeem:@"value2"];
+}
+
+// Multiple promises can be composed by redeeming a promise from another promise.
+- (void)testChainingFutures
+{
+    FCPromise *promise1 = [[[FCPromise alloc] init] autorelease];
+    FCPromise *promise2 = [[[FCPromise alloc] init] autorelease];
+    __block int count = 0;
+
+    [promise1 onRedeem:^(id value) {
+        STAssertEqualObjects(@"value1", value, @"value of promise1 = %@", value);
+        count ++;
+        [promise2 redeem:@"value2"];
+    }];
+
+    [promise2 onRedeem:^(id value) {
+        STAssertEqualObjects(@"value2", value, @"value of promise2 = %@", value);
+        count ++;
+    }];
+
+    [promise1 redeem:@"value1"];
+    STAssertEquals(2, count, @"redeem count = %d", count);
+}
+
+// A typical use case is that redeeming a promise asynchronously by using Grand Central Dispatch.
+- (void)testRedeemFromDispatchGlobalQueue
+{
+    FCPromise *promise1 = [[[FCPromise alloc] init] autorelease];
+    __block int count = 0;
+    
+    [promise1 onRedeem:^(id value) {
+        STAssertEqualObjects(value, @"value1", @"value of promise1 = %@", value);
+        count ++;
+    }];
+
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [promise1 redeem:@"value1"];
+    });
+
+    STAssertEquals(count, 1, @"redeemed count");
 }
 
 @end
